e6b516956df3bb096fe775e271da584437dcc803
evantcsm <evan.beilke@dsa011080.docusignhq.com>
2017-03-13 14:14:33 -0700

    Initial JS file
    Initial JS file

---
+++ autoProvision.js
@@ -0,0 +1,144 @@
+// Create users for your account (Node.js)
+
+// To run this sample
+//  1. Copy the file to your local machine and give .js extension (i.e. example.js)
+//  2. Change "***" to appropriate values
+//  3. Install async and request packages
+//     npm install async
+//     npm install request
+//     npm install fs
+//  4. execute
+//     node example.js 
+//
+
+var     async = require("async"),		// async module
+        request = require("request"),		// request module
+    	fs = require("fs");			// fs module
+
+var 	email = "",				// your account email
+    	password = "",			// your account password
+    	integratorKey = "",			// your Integrator Key (found on the Preferences -> API page)
+    	baseUrl = ""; 				// we will retrieve this through the Login call
+
+var numberOfUsers = 10;      // the number of users you want to create
+
+var emailAddress='', userName='';
+var usersArray = [];       // array that we will throw the stringified values in
+for(var i = 0; i<numberOfUsers;i++){        
+emailAddress += "example"+i+"@gmail.com";    // email template that we will use and iterate a number for i. 
+userName += "Test User+"+i;                    // username template that we will use and iterate a number for i. 
+
+var apiUsers = {
+	
+           
+            "userName": userName,
+            "userSettings":[{
+      			"name":"apiAccountWideAccess",
+      			"value":"true"
+    		},
+    		{
+    			"name":"allowSendOnBehalfOf",
+      			"value":"true"
+    		}],
+            "email": emailAddress,
+            "forgottenPasswordInfo": {
+                "forgottenPasswordAnswer1": "lsdjfqwerwers",
+                "forgottenPasswordAnswer2": "zxvsdfwerasdfas",
+                "forgottenPasswordAnswer3": "jhhkjdfiuidysfuiy",
+                "forgottenPasswordAnswer4": "qswervtrnynt",
+                "forgottenPasswordQuestion1": "adfwerqxvwsedf",
+                "forgottenPasswordQuestion2": "asdfawerzbfad",
+                "forgottenPasswordQuestion3": "sadfuioyiuwqer",
+                "forgottenPasswordQuestion4": "asdfnbmnwqerqwe"
+            },
+            
+            "password": "123456"
+        } ;
+        usersArray.push(JSON.stringify(apiUsers));    // adding the stringified verion to the array
+
+        emailAddress='';     // reseting the values after each loop iteration 
+        userName='';         // reseting the values after each loop iteration 
+
+
+};
+async.waterfall(
+  [
+    /////////////////////////////////////////////////////////////////////////////////////
+    // Step 1: Login (used to retrieve your accountId and baseUrl)
+    /////////////////////////////////////////////////////////////////////////////////////
+    function(next) {
+		var url = "https://demo.docusign.net/restapi/v2/login_information";
+		var body = "";	// no request body for login api call
+		
+		// set request url, method, body, and headers
+		var options = initializeRequest(url, "GET", body, email, password);
+		
+		// send the request...
+		request(options, function(err, res, body) {
+			if(!parseResponseBody(err, res, body)) {
+				return;
+			}
+			baseUrl = JSON.parse(body).loginAccounts[0].baseUrl;
+			next(null); // call next function
+		});
+	},
+    
+    /////////////////////////////////////////////////////////////////////////////////////
+    // Step 2: Create new users
+    /////////////////////////////////////////////////////////////////////////////////////
+    function(next) {   	
+    	var url = baseUrl + "/users";
+    	
+    	// defining the body with the usersArray
+		var body = '{"newUsers": ['+usersArray+']}';
+		
+		// set request url, method, body, and headers
+		var options = initializeRequest(url, "POST", body, email, password);
+	
+		// set Content-Type header to "application/json"
+		options.headers["Content-Type"] = "application/json";
+
+		// send the request...
+		request(options, function(err, res, body) {
+			parseResponseBody(err, res, body);
+		});
+	} // end function    
+]);
+
+//***********************************************************************************************
+// --- HELPER FUNCTIONS ---
+//***********************************************************************************************
+function initializeRequest(url, method, body, email, password) {	
+	var options = {
+		"method": method,
+		"uri": url,
+		"body": body,
+		"headers": {}
+	};
+	addRequestHeaders(options, email, password);
+	return options;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+function addRequestHeaders(options, email, password) {	
+	// JSON formatted authentication header (XML format allowed as well)
+	dsAuthHeader = JSON.stringify({
+		"Username": email,
+		"Password": password, 
+		"IntegratorKey": integratorKey	// global
+	});
+	// DocuSign authorization header
+	options.headers["X-DocuSign-Authentication"] = dsAuthHeader;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+function parseResponseBody(err, res, body) {
+	console.log("\r\nAPI Call Result: \r\n", JSON.parse(body));
+	if( res.statusCode != 200 && res.statusCode != 201)	{ // success statuses
+		console.log("Error calling webservice, status is: ", res.statusCode);
+		console.log("\r\n", err);
+		return false;
+	}
+	return true;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
